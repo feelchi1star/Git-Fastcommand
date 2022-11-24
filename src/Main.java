1379ed5d055e245f85a8e2d8c771f24d09833849
feelchi1star@gmail.com <feelchi1star@gmail.com>
2022-11-24 12:01:16 +0100

    Dowhile loops Class Created

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+String john,peter;
+
+   int counter =0;
+   while(counter <5){
+       Print(counter);
+       counter++;
+   }
+
+   john="moses";
+   peter="peter";
+        System.out.println(john);
+
+        System.out.println(peter);
+
+    }
+
+
+
+    private static void Print(int x){
+
+        System.out.println(x +" Hello From Second method =" + (12+26));
+
+
     }
 }
